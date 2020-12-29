cfbc953280afb7623d5d908e8ec5573f7b7d1801
Sk Rockybul Hasan Rasel <sk.rasel.cse@gmail.com>
2020-12-29 16:02:29 +0600

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'FadeAnimation.dart';
+
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Nunito'),
       home: HomePage(),
     ));
 
@@ -10,22 +13,229 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  PageController _pageController;
+  int totalPage = 4;
+
+  void _onScroll() {
+    print('Rasel');
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: _pageController,
         children: [
-          Container(
-            color: Colors.blue,
-          ),
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
+          makePage(
+              page: 1,
+              image: 'assets/images/four.jpg',
+              title: 'Bag',
+              description:
+                  'Be magical for whoever remains, because each has been remembered with moonlight.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.'),
+          makePage(
+              page: 2,
+              image: 'assets/images/two.jpg',
+              title: 'Watch',
+              description:
+                  'Toruss sunt cedriums de regius musa. Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.'),
+          makePage(
+              page: 3,
+              image: 'assets/images/one.jpg',
+              title: 'Pie Cake',
+              description:
+                  'Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.'),
+          makePage(
+              page: 4,
+              image: 'assets/images/three.jpg',
+              title: 'Bottle',
+              description:
+                  'Fire me freebooter, ye scurvy cockroach! The starlight travel is a senior cosmonaut.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.Onion can be jumbled with springy turkey, also try flavoring the fritters with olive oil.'),
         ],
+      ),
+    );
+  }
+
+  Widget makePage({image, title, description, page}) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+            0.1,
+            0.8
+          ], colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.2),
+          ]),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  FadeAnimation(
+                    2,
+                    Text(
+                      page.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '/' + totalPage.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeAnimation(
+                      .2,
+                      Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                      .3,
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Text(
+                            '4.0',
+                            style: TextStyle(
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text(
+                            '(2336)',
+                            style: TextStyle(
+                              color: Colors.white38,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                      1.3,
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: Text(
+                          description,
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(.7),
+                            height: 1.9,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                      2.5,
+                      Text(
+                        'READ MORE',
+                        style: TextStyle(
+                          color: Colors.deepOrangeAccent,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
